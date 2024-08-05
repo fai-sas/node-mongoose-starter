a4c37c252c3ed315fd231bc945511ed930d28388
fai-sas <craftedbyfaisal@gmail.com>
2024-08-05 10:20:31 +0600

    connected to server

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+import cors from 'cors'
+import express, { Application, Request, Response } from 'express'
+import cookieParser from 'cookie-parser'
+import router from './app/routes'
+import globalErrorHandler from './app/middlewares/globalErrorhandler'
+import notFound from './app/middlewares/notFound'
+
+const app: Application = express()
+
+//parsers
+app.use(express.json())
+app.use(cors())
+app.use(cookieParser())
+
+// application routes
+app.use('/api/v1', router)
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Welcome to Node Mongoose Starter Pack')
+})
+
+app.use(globalErrorHandler)
+app.use(notFound)
+
+export default app
---
+++ src/app/config/index.ts
@@ -0,0 +1,20 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+export default {
+  port: process.env.PORT,
+  database_url: process.env.DATABASE_URL,
+  bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
+  NODE_ENV: process.env.NODE_ENV,
+  default_password: process.env.DEFAULT_PASS,
+  jwt_access_secret: process.env.JWT_ACCESS_SECRET,
+  jwt_refresh_secret: process.env.JWT_REFRESH_SECRET,
+  jwt_access_expires_in: process.env.JWT_ACCESS_EXPIRES_IN,
+  jwt_refresh_expires_in: process.env.JWT_REFRESH_EXPIRES_IN,
+  reset_password_ui_link: process.env.RESET_PASSWORD_UI_LINK,
+  email_address: process.env.EMAIl_ADDRESS,
+  email_password: process.env.EMAIL_PASSWORD,
+  cloudinary_cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  cloudinary_api_key: process.env.CLOUDINARY_API_KEY,
+  cloudinary_api_secret: process.env.CLOUDINARY_API_SECRET,
+}
